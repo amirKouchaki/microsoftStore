a087ba6ab7e9fe44dd2273236e6f3b14f80a495f
amir kouchaki <amirkouchaki1@gmail.com>
2021-09-10 23:52:13 +0430

    added views for login & register & index

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,46 @@
+<x-guest-layout>
+    <x-slot name="head">
+        <title>Sign in | Microsoft store</title>
+        <link rel="stylesheet" href="/css/css reset.css" />
+        <link rel="stylesheet" href="/css/login/style.css" />
+        <!--ubuntu font cdn-->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap"
+            rel="stylesheet"
+        />
+    </x-slot>
+
+    <main id="container">
+        <img
+            src="https://winaero.com/blog/wp-content/uploads/2021/07/Microsoft_Store_icon.png"
+            alt="Logo"
+        />
+        <h1>Sign in</h1>
+        <form action="{{route('login')}}" method="post">
+            @csrf
+            <section>
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Username"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                />
+            </section>
+            <button type="submit">Sign in</button>
+        </form>
+        <a href="{{route('register')}}">Create an account</a>
+    </main>
+
+    <x-slot name="footerScripts">
+        <script src="/js/login/script.js"></script>
+    </x-slot>
+
+</x-guest-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,76 @@
+<x-guest-layout>
+    <x-slot name="head">
+        <title>Sign up| Microsoft store</title>
+        <link rel="stylesheet" href="/css/css reset.css" />
+        <link rel="stylesheet" href="/css/register/style.css" />
+        <!--ubuntu font cdn-->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap"
+            rel="stylesheet"
+        />
+    </x-slot>
+
+    <div id="container">
+        <img
+            src="https://winaero.com/blog/wp-content/uploads/2021/07/Microsoft_Store_icon.png"
+            alt="Logo"
+        />
+        <h1>Sign up</h1>
+        <form action="#" method="POST">
+            <section>
+                <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    placeholder="First name"
+                    required
+                />
+                <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    placeholder="Last name"
+                    required
+                />
+            </section>
+            <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                required
+            />
+            <span>Email will be used for your username.</span>
+            <div id="pass">
+                <section>
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="confirm-password"
+                        id="confirm-password"
+                        placeholder="Confirm"
+                        required
+                    />
+                </section>
+                <span
+                >Use at least 8 characters with a mix of letters, numbers &
+            symbols</span
+                >
+            </div>
+            <button type="submit">Sign up</button>
+        </form>
+        <a href="{{route('login')}}">I have an account.</a>
+    </div>
+    <x-slot name="footerScripts">
+        <script src="/js/register/script.js"></script>
+    </x-slot>
+
+</x-guest-layout>
