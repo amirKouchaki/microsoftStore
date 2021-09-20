a1058c4558d2db39fc4c8bc706b646a31f9b31d8
amir kouchaki <amirkouchaki1@gmail.com>
2021-09-20 12:25:47 +0430

    added database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create(['email' => 'test@gmail.com']);
+        User::factory()->create(['email' => 'test@test.com','password' => Hash::make(12345678)]);
     }
 }
